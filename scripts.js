07e66872349b32ee7b024bf57a3df755680c1389
purrogrammer <margueritenyc@hotmail.com>
2019-10-31 16:50:23 -0700

    add and revise JS scripts

--- scripts.js
+++ scripts.js
@@ -29,8 +29,61 @@ class Match {
         this.audioController = new AudioController();
     }
 
+    startGame() {
+        this.totalClicks=0;
+        this.timeRemaining = this.totalTime;
+        this.cardToCheck = null;
+        this.matchedCards = [];
+        this.busy = true;
+        setTimeout(() => {
+            this.shuffleCards(this.CardsArrray);
+            this.countdown = this.startCountdown();
+            this.busy = false;
+         }, 500); 
+         this.hideCards();
+         this.timer.innerText = this.timeRemaining;
+         this.ticker.innerText = this.totalClicks; 
+    }
+    startCountdown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0)
+                this.gameOver(); 
+        }, 1000);
+    }
 
+    gameOver() {
+        //code to go here
+    }
 
+    hideCards() {
+        this.cardsArray.forEach(() => 
+        //more to go here
+    }
 
-    
-}
+    flipCard(card) {
+            //code to go here
+
+    }
+
+    checkIfCardMatch(card) {
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+         this.cardMatch(card, this.cardToCheck);
+        else
+            this.cardNoMatch(card, this.cardToCheck);
+        this.cardToCheck = null;             
+    }
+    cardMatch(card1, card2) {
+        //something something leading to "victory" text & sound
+
+    }
+
+    cardNoMatch(card1, card2) {
+        //something something "womp womp" sound and reset
+
+    }
+
+    shuffleCards() { 
+    //Fisher-Yates loop to go here
+    }
