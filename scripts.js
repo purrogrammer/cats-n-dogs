83a2e6b4e9ef735ec1b06d2fcfb0687e3deaf1b9
purrogrammer <margueritenyc@hotmail.com>
2019-10-31 16:03:53 -0700

    add sound files and initial JS scripts

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
-alert("Scripts are working!"); 
+class AudioController {
+    constructor() {
+        this.flipSound = new Audio('Assets/sounds/flip.wav');
+        this.matchSound = new Audio('Assets/sounds/match.wav');
+        this.victorySound = new Audio('Assets/sounds/victory.wav');
+        this.gameOverSound = new Audio('Assets/sounds/gameOver.wav');
+
+    flip() {
+        this.flipSound.play();
+    }
+    match() {
+        this.matchSound.play();
+    }
+    victory() {
+        this.victorySound.play();
+    }
+    gameOver() {
+        this.gameOverSound.play();
+    }
+}
+
+class Match {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining')
+        this.ticker = document.getElementById('flips');
+        this.audioController = new AudioController();
+    }
+
+
+
+
+    
+}
